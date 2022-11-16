6540e4cce1710bbbe699197a8d2e3bf8a6cf057f
Luke Van In <lukevanin@gmail.com>
2022-11-16 09:05:32 +0200

    Initial Commit

---
+++ Printy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Printy
+//
+//  Created by Luke Van In on 2022/11/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
